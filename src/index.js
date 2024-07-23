776bf866fc3d3905d94705593855a61f59b65d50
Amanfromearth <anurag3b@gmail.com>
2024-07-23 19:36:23 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  register(/*{ strapi }*/) {},
+
+  async bootstrap({ strapi }) {
+    const io = require('socket.io')(strapi.server.httpServer, {
+      cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+      }
+    });
+
+    io.on('connection', (socket) => { 
+      console.log('A user connected');
+
+      socket.on('sendMessage', async (data) => {
+        console.log('Received data:', data);
+        try {
+          const entry = await strapi.entityService.create('api::message.message', {
+            data: data
+          });
+          socket.emit('messageReceived', entry);
+        } catch (error) {
+          console.error('Error creating message:', error);
+          socket.emit('error', { message: 'Failed to create message' });
+        }
+      });
+
+      socket.on('disconnect', () => {
+        console.log('A user disconnected');
+      });
+    });
+  },
+};
